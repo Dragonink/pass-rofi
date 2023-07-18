411e3969118f29f70bb0e61ae6b9411d8d12dada
Tanguy Berthoud <tanguy.berthoud@orolia2s.com>
2023-07-18 14:41:00 +0200

    WIP

--- default.nix
+++ default.nix
@@ -1,20 +1,28 @@
-{ lib, stdenv, pass, rofi, xdotool, xorg, busybox, makeWrapper }: stdenv.mkDerivation rec {
-	pname = "pass-rofi";
-	version = "1.0";
+{ pkgs ? import <nixpkgs> {} }:
+  let
+    lib = pkgs.lib;
+    stdenv = pkgs.stdenv;
+  in stdenv.mkDerivation rec {
+    pname = "pass-rofi";
+    version = "1.0";
 
-	src = ./.;
+    src = ./.;
 
-	buildInputs = [ pass rofi xdotool xorg.setxkbmap busybox ];
-	nativeBuildInputs = [ makeWrapper ];
-	
-	installPhase = ''
-		mkdir --parents $out/bin
-		cp ${src}/pass-rofi $out/bin/
-		wrapProgram $out/bin/pass-rofi \
-			--prefix PATH : "${lib.makeBinPath buildInputs}"
-		
-		PASS_EXTENSIONS='lib/password-store/extensions'
-		mkdir --parents $out/$PASS_EXTENSIONS
-		ln -s $out/bin/pass-rofi $out/$PASS_EXTENSIONS/rofi.bash
-	'';
-}
+    buildInputs = with pkgs; [ pass rofi xdotool xorg.setxkbmap busybox ];
+    nativeBuildInputs = with pkgs; [ makeWrapper ];
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir --parents $out/bin
+      cp pass-rofi $out/bin/
+      wrapProgram $out/bin/pass-rofi \
+        --prefix PATH : "${lib.makeBinPath buildInputs}"
+
+      PASS_EXTENSIONS='lib/password-store/extensions'
+      mkdir --parents $out/$PASS_EXTENSIONS
+      ln --symbolic $out/bin/pass-rofi $out/$PASS_EXTENSIONS/rofi.bash
+
+      runHook postInstall
+    '';
+  }
--- flake.nix
+++ flake.nix
@@ -1,24 +1,19 @@
 {
-	description = "Pass extension that provides a UI using rofi";
+  description = "Pass extension that provides a UI using rofi";
 
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-		flake-utils.url = "github:numtide/flake-utils";
-	};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-	outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-		let
-			pkgs = nixpkgs.legacyPackages.${system};
-		in rec {
-			packages = rec {
-				pass-rofi = pkgs.callPackage ./default.nix {};
-				default = pass-rofi;
-			};
-			legacyPackages = packages;
-			apps = rec {
-				pass-rofi = flake-utils.lib.mkApp { drv = self.packages.${system}.pass-rofi; };
-				default = pass-rofi;
-			};
-		}
-	);
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages = rec {
+        pass-rofi = import ./default.nix { inherit pkgs; };
+        default = pass-rofi;
+      };
+    }
+  );
 }
